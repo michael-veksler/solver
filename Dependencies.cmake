24f665d2415b45711402ec7576e3b2d0400d8758
Michael Veksler <mickey.veksler@gmail.com>
2023-10-24 00:36:44 +0300

    Add boost::throw_exception

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -73,6 +73,14 @@ function(solver_setup_dependencies)
       GIT_TAG "boost-${BOOST_VERSION}"
     )
   endif()
+  if(NOT TARGET Boost::throw_exception)
+    CPMAddPackage(
+      NAME boost_throw_exception
+      VERSION ${BOOST_VERSION}
+      GITHUB_REPOSITORY "boostorg/throw_exception"
+      GIT_TAG "boost-${BOOST_VERSION}"
+    )
+  endif()
 
   if(NOT TARGET Catch2::Catch2WithMain)
     cpmaddpackage("gh:catchorg/Catch2@3.3.2")
