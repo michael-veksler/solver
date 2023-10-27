39687029b008351798e1a1024e34b61655a3df13
Michael Veksler <mickey.veksler@gmail.com>
2023-10-27 21:41:14 +0300

    Create a solver executable
    The solver executable runs a dimacs parser and tries to solve
    the problem. It prints the outcome.

--- include/solver/sat_types.hpp
+++ include/solver/sat_types.hpp
@@ -2,11 +2,19 @@
 #define SAT_TYPES_HPP
 
 #include <cinttypes>
+#include <string>
 
 namespace solver {
 
 enum class solve_status : int8_t { SAT, UNSAT, UNKNOWN };
-
+inline std::string to_string(solve_status status) {
+  switch (status) {
+    case solve_status::SAT:     return "SAT";
+    case solve_status::UNSAT:   return "UNSAT";
+    case solve_status::UNKNOWN: return "UNKNOWN";
+  }
+  return "Unknown";
+}
 /**
  * @brief Literal types used for
  *
--- src/solver_exe/main.cpp
+++ src/solver_exe/main.cpp
@@ -1,312 +1,109 @@
+#include "solver/cdcl_sat.hpp"
+#include "solver/sat_types.hpp"
+#include "solver/trivial_sat.hpp"
 #include <array>
+#include <fmt/core.h>
 #include <functional>
 #include <iostream>
 #include <optional>
-
-#include <random>
-
+#include <map>
+#include <ranges>
+#include <fstream>
 #include <CLI/CLI.hpp>
-#include <ftxui/component/captured_mouse.hpp>// for ftxui
-#include <ftxui/component/component.hpp>// for Slider
-#include <ftxui/component/screen_interactive.hpp>// for ScreenInteractive
 #include <spdlog/spdlog.h>
 
-#include <lefticus/tools/non_promoting_ints.hpp>
+#include <solver/dimacs_parser.hpp>
 
 // This file will be generated automatically when cur_you run the CMake
 // configuration step. It creates a namespace called `solver`. You can modify
 // the source template at `configured_files/config.hpp.in`.
 #include <internal_use_only/config.hpp>
 
-template<std::size_t Width, std::size_t Height> struct GameBoard
-{
-  static constexpr std::size_t width = Width;
-  static constexpr std::size_t height = Height;
-
-  std::array<std::array<std::string, height>, width> strings;
-  std::array<std::array<bool, height>, width> values{};
-
-  std::size_t move_count{ 0 };
-
-  std::string &get_string(std::size_t cur_x, std::size_t cur_y) { return strings.at(cur_x).at(cur_y); }
-
-
-  void set(std::size_t cur_x, std::size_t cur_y, bool new_value)
-  {
-    get(cur_x, cur_y) = new_value;
-
-    if (new_value) {
-      get_string(cur_x, cur_y) = " ON";
-    } else {
-      get_string(cur_x, cur_y) = "OFF";
-    }
-  }
-
-  void visit(auto visitor)
-  {
-    for (std::size_t cur_x = 0; cur_x < width; ++cur_x) {
-      for (std::size_t cur_y = 0; cur_y < height; ++cur_y) { visitor(cur_x, cur_y, *this); }
-    }
-  }
-
-  [[nodiscard]] bool get(std::size_t cur_x, std::size_t cur_y) const { return values.at(cur_x).at(cur_y); }
-
-  [[nodiscard]] bool &get(std::size_t cur_x, std::size_t cur_y) { return values.at(cur_x).at(cur_y); }
-
-  GameBoard()
-  {
-    visit([](const auto cur_x, const auto cur_y, auto &gameboard) { gameboard.set(cur_x, cur_y, true); });
-  }
-
-  void update_strings()
-  {
-    for (std::size_t cur_x = 0; cur_x < width; ++cur_x) {
-      for (std::size_t cur_y = 0; cur_y < height; ++cur_y) { set(cur_x, cur_y, get(cur_x, cur_y)); }
-    }
-  }
-
-  void toggle(std::size_t cur_x, std::size_t cur_y) { set(cur_x, cur_y, !get(cur_x, cur_y)); }
-
-  void press(std::size_t cur_x, std::size_t cur_y)
-  {
-    ++move_count;
-    toggle(cur_x, cur_y);
-    if (cur_x > 0) { toggle(cur_x - 1, cur_y); }
-    if (cur_y > 0) { toggle(cur_x, cur_y - 1); }
-    if (cur_x < width - 1) { toggle(cur_x + 1, cur_y); }
-    if (cur_y < height - 1) { toggle(cur_x, cur_y + 1); }
-  }
-
-  [[nodiscard]] bool solved() const
-  {
-    for (std::size_t cur_x = 0; cur_x < width; ++cur_x) {
-      for (std::size_t cur_y = 0; cur_y < height; ++cur_y) {
-        if (!get(cur_x, cur_y)) { return false; }
-      }
-    }
-
-    return true;
-  }
-};
-
-
-void consequence_game()
-{
-  auto screen = ftxui::ScreenInteractive::TerminalOutput();
-
-  GameBoard<3, 3> game_board;
-
-  std::string quit_text;
-
-  const auto update_quit_text = [&quit_text](const auto &game_board_param) {
-    quit_text = fmt::format("Quit ({} moves)", game_board_param.move_count);
-    if (game_board_param.solved()) { quit_text += " Solved!"; }
+namespace {
+  enum class solver_kind {
+    trivial_sat = 0,
+    cdcl_sat = 1
   };
-
-  const auto make_buttons = [&] {
-    std::vector<ftxui::Component> buttons;
-    for (std::size_t cur_x = 0; cur_x < game_board.width; ++cur_x) {
-      for (std::size_t cur_y = 0; cur_y < game_board.height; ++cur_y) {
-        buttons.push_back(ftxui::Button(&game_board.get_string(cur_x, cur_y), [=, &game_board] {
-          if (!game_board.solved()) { game_board.press(cur_x, cur_y); }
-          update_quit_text(game_board);
-        }));
-      }
-    }
-    return buttons;
-  };
-
-  auto buttons = make_buttons();
-
-  auto quit_button = ftxui::Button(&quit_text, screen.ExitLoopClosure());
-
-  auto make_layout = [&] {
-    std::vector<ftxui::Element> rows;
-
-    std::size_t idx = 0;
-
-    for (std::size_t cur_x = 0; cur_x < game_board.width; ++cur_x) {
-      std::vector<ftxui::Element> row;
-      for (std::size_t cur_y = 0; cur_y < game_board.height; ++cur_y) {
-        row.push_back(buttons[idx]->Render());
-        ++idx;
-      }
-      rows.push_back(ftxui::hbox(std::move(row)));
-    }
-
-    rows.push_back(ftxui::hbox({ quit_button->Render() }));
-
-    return ftxui::vbox(std::move(rows));
-  };
-
-
-  static constexpr int randomization_iterations = 100;
-  static constexpr int random_seed = 42;
-
-  std::mt19937 gen32{ random_seed };// NOLINT fixed seed
-
-  // NOLINTNEXTLINE This cannot be const
-  std::uniform_int_distribution<std::size_t> cur_x(static_cast<std::size_t>(0), game_board.width - 1);
-  // NOLINTNEXTLINE This cannot be const
-  std::uniform_int_distribution<std::size_t> cur_y(static_cast<std::size_t>(0), game_board.height - 1);
-
-  for (int i = 0; i < randomization_iterations; ++i) { game_board.press(cur_x(gen32), cur_y(gen32)); }
-  game_board.move_count = 0;
-  update_quit_text(game_board);
-
-  auto all_buttons = buttons;
-  all_buttons.push_back(quit_button);
-  auto container = ftxui::Container::Horizontal(all_buttons);
-
-  auto renderer = ftxui::Renderer(container, make_layout);
-
-  screen.Loop(renderer);
 }
 
-struct Color
+namespace {
+template <typename solver_type>
+class solver_main
 {
-  lefticus::tools::uint_np8_t R{ static_cast<std::uint8_t>(0) };
-  lefticus::tools::uint_np8_t G{ static_cast<std::uint8_t>(0) };
-  lefticus::tools::uint_np8_t B{ static_cast<std::uint8_t>(0) };
-};
+public:
+  template <typename ...Args>
+  explicit solver_main(Args && ...args) : m_solver(std::forward<Args>(args)...) {
+  }
 
-// A simple way of representing a bitmap on screen using only characters
-struct Bitmap : ftxui::Node
-{
-  Bitmap(std::size_t width, std::size_t height)// NOLINT same typed parameters adjacent to each other
-    : width_(width), height_(height)
-  {}
+  void solve(const std::string & filename) {
+    parse(filename);
+    const solver::solve_status status = m_solver.solve();
+    if (status == solver::solve_status::SAT) {
+      fmt::print("SAT");
+      // When we upgrade to clang-16, use std::views::drop(1) instead.
+      for (unsigned i=1; i != m_variables.size(); ++i) {
+        auto var = m_variables[i];
+        fmt::print(" v{}={}", var, static_cast<int>(m_solver.get_current_domain(var).contains(true)));
+      }
+      fmt::print("\n");
+    } else {
+      fmt::println("{}", to_string(status));
+    }
 
-  Color &at(std::size_t cur_x, std::size_t cur_y) { return pixels.at(width_ * cur_y + cur_x); }
+  }
+private:
 
-  void ComputeRequirement() override
+  /**
+   * @brief Get a lambda that gets literals and constructs clauses.
+   *
+   * @return A callable that receives literals and adds a clause with those literals to m_solver.
+   */
+  auto get_clause_constructor()
   {
-    requirement_ = ftxui::Requirement{
-      .min_x = static_cast<int>(width_), .min_y = static_cast<int>(height_ / 2), .selected_box{ 0, 0, 0, 0 }
+    return [this](const std::vector<int> &literals) {
+       auto & clause = m_solver.add_clause();
+       for (const int literal: literals) {
+        const bool is_positive = literal > 0;
+        const auto var_index = static_cast<unsigned>(is_positive ? literal : -literal);
+        const unsigned var =  m_variables.at(var_index);
+        clause.add_literal(var, is_positive);
+       }
     };
   }
 
-  void Render(ftxui::Screen &screen) override
+  /**
+   * @brief Get a lambda that gets num of variables and clauses and initializes m_solver.
+   *
+   * @return A callable that initializes the solver with variables.
+   */
+  auto get_problem_constructor()
   {
-    for (std::size_t cur_x = 0; cur_x < width_; ++cur_x) {
-      for (std::size_t cur_y = 0; cur_y < height_ / 2; ++cur_y) {
-        auto &pixel = screen.PixelAt(box_.x_min + static_cast<int>(cur_x), box_.y_min + static_cast<int>(cur_y));
-        pixel.character = "▄";
-        const auto &top_color = at(cur_x, cur_y * 2);
-        const auto &bottom_color = at(cur_x, cur_y * 2 + 1);
-        pixel.background_color = ftxui::Color{ top_color.R.get(), top_color.G.get(), top_color.B.get() };
-        pixel.foreground_color = ftxui::Color{ bottom_color.R.get(), bottom_color.G.get(), bottom_color.B.get() };
-      }
-    }
+    return [this](unsigned count_vars, unsigned count_clauses) {
+      m_solver.reserve_vars(count_vars);
+      m_variables.reserve(count_vars + 1);
+      m_variables.push_back(0);
+
+      std::generate_n(std::back_inserter(m_variables), count_vars, [this] { return m_solver.add_var(); });
+      m_solver.reserve_clauses(count_clauses);
+    };
+
   }
 
-  [[nodiscard]] auto width() const noexcept { return width_; }
+  void parse(const std::string & filename) {
+    std::ifstream input_file(filename);
+    auto getline = [&input_file](std::string &line) {
+      std::getline(input_file, line);
+      return bool(input_file);
+    };
 
-  [[nodiscard]] auto height() const noexcept { return height_; }
+    solver::dimacs_parser file_parser(getline);
 
-  [[nodiscard]] auto &data() noexcept { return pixels; }
+    file_parser.parse(get_problem_constructor(), get_clause_constructor());
+  }
 
-private:
-  std::size_t width_;
-  std::size_t height_;
-
-  std::vector<Color> pixels = std::vector<Color>(width_ * height_, Color{});
+  std::vector<typename solver_type::variable_handle> m_variables;
+  solver_type m_solver;
 };
-
-void game_iteration_canvas()
-{
-  // this should probably have a `bitmap` helper function that does what cur_you expect
-  // similar to the other parts of FTXUI
-  auto bm = std::make_shared<Bitmap>(50, 50);// NOLINT magic numbers
-  auto small_bm = std::make_shared<Bitmap>(6, 6);// NOLINT magic numbers
-
-  double fps = 0;
-
-  std::size_t max_row = 0;
-  std::size_t max_col = 0;
-
-  // to do, add total game time clock also, not just current elapsed time
-  auto game_iteration = [&](const std::chrono::steady_clock::duration elapsed_time) {
-    // in here we simulate however much game time has elapsed. Update animations,
-    // run character AI, whatever, update stats, etc
-
-    // this isn't actually timing based for now, it's just updating the display however fast it can
-    fps = 1.0
-          / (static_cast<double>(std::chrono::duration_cast<std::chrono::microseconds>(elapsed_time).count())
-             / 1'000'000.0);// NOLINT magic numbers
-
-    for (std::size_t row = 0; row < max_row; ++row) {
-      for (std::size_t col = 0; col < bm->width(); ++col) { ++(bm->at(col, row).R); }
-    }
-
-    for (std::size_t row = 0; row < bm->height(); ++row) {
-      for (std::size_t col = 0; col < max_col; ++col) { ++(bm->at(col, row).G); }
-    }
-
-    // for the fun of it, let's have a second window doing interesting things
-    auto &small_bm_pixel =
-      small_bm->data().at(static_cast<std::size_t>(elapsed_time.count()) % small_bm->data().size());
-
-    switch (elapsed_time.count() % 3) {
-    case 0:
-      small_bm_pixel.R += 11;// NOLINT Magic Number
-      break;
-    case 1:
-      small_bm_pixel.G += 11;// NOLINT Magic Number
-      break;
-    case 2:
-      small_bm_pixel.B += 11;// NOLINT Magic Number
-      break;
-    }
-
-
-    ++max_row;
-    if (max_row >= bm->height()) { max_row = 0; }
-    ++max_col;
-    if (max_col >= bm->width()) { max_col = 0; }
-  };
-
-  auto screen = ftxui::ScreenInteractive::TerminalOutput();
-
-  int counter = 0;
-
-  auto last_time = std::chrono::steady_clock::now();
-
-  auto make_layout = [&] {
-    // This code actually processes the draw event
-    const auto new_time = std::chrono::steady_clock::now();
-
-    ++counter;
-    // we will dispatch to the game_iteration function, where the work happens
-    game_iteration(new_time - last_time);
-    last_time = new_time;
-
-    // now actually draw the game elements
-    return ftxui::hbox({ bm | ftxui::border,
-      ftxui::vbox({ ftxui::text("Frame: " + std::to_string(counter)),
-        ftxui::text("FPS: " + std::to_string(fps)),
-        small_bm | ftxui::border }) });
-  };
-
-  auto renderer = ftxui::Renderer(make_layout);
-
-
-  std::atomic<bool> refresh_ui_continue = true;
-
-  // This thread exists to make sure that the event queue has an event to
-  // process at approximately a rate of 30 FPS
-  std::thread refresh_ui([&] {
-    while (refresh_ui_continue) {
-      using namespace std::chrono_literals;
-      std::this_thread::sleep_for(1.0s / 30.0);// NOLINT magic numbers
-      screen.PostEvent(ftxui::Event::Custom);
-    }
-  });
-
-  screen.Loop(renderer);
-
-  refresh_ui_continue = false;
-  refresh_ui.join();
 }
 
 // NOLINTNEXTLINE(bugprone-exception-escape)
@@ -315,35 +112,29 @@ int main(int argc, const char **argv)
   try {
     CLI::App app{ fmt::format("{} version {}", solver::cmake::project_name, solver::cmake::project_version) };
 
-    std::optional<std::string> message;
-    app.add_option("-m,--message", message, "A message to print back out");
-    bool show_version = false;
-    app.add_flag("--version", show_version, "Show version information");
-
-    bool is_turn_based = false;
-    auto *turn_based = app.add_flag("--turn_based", is_turn_based);
-
-    bool is_loop_based = false;
-    auto *loop_based = app.add_flag("--loop_based", is_loop_based);
-
-    turn_based->excludes(loop_based);
-    loop_based->excludes(turn_based);
-
+    solver_kind requested_solver = solver_kind::cdcl_sat;
+    const std::map<std::string, solver_kind> solver_kind_map = {{"trivial_sat", solver_kind::trivial_sat}, {"cdcl_sat", solver_kind::cdcl_sat}};
+    app.add_option("--solver", requested_solver, "The solver to use")
+      ->required()
+      ->transform(CLI::CheckedTransformer(solver_kind_map, CLI::ignore_case));
+    std::string input;
+    app.add_option("--input,input", input, "Input file")->required();
 
     CLI11_PARSE(app, argc, argv);
-
-    if (show_version) {
-      fmt::print("{}\n", solver::cmake::project_version);
-      return EXIT_SUCCESS;
+    switch (requested_solver) {
+      case solver_kind::cdcl_sat:
+        solver_main<solver::cdcl_sat>().solve(input);
+        break;
+      case solver_kind::trivial_sat:
+        solver_main<solver::trivial_sat>().solve(input);
+        break;
     }
 
-    if (is_turn_based) {
-      consequence_game();
-    } else {
-      game_iteration_canvas();
-    }
 
-  } catch (const std::exception &e) {
+  } catch (CLI::ParseError &e) {
+    fmt::print("=== parse error ==== {}", e.what());
+    return 1;
+  }catch (const std::exception &e) {
     spdlog::error("Unhandled exception in main: {}", e.what());
   }
 }
