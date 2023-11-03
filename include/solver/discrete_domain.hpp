eafb94d85b1a763ad5fac30e92ba0aa59c332bda
Michael Veksler <mickey.veksler@gmail.com>
2023-11-03 10:17:28 +0200

    Implemented discrete_domain (#17)
    * Implemented discrete_domain
    
    Note: The boost::icl module has overflow bugs for numeric_limits<ValueT>::max(),
               and this value has to be avoided until the bugs are fixed.
    
    * Save artifacts on failure
    When test failure occurs, save the logs of the test run.

---
+++ include/solver/discrete_domain.hpp
@@ -0,0 +1,117 @@
+#ifndef DISCRETE_DOMAIN_HPP
+#define DISCRETE_DOMAIN_HPP
+
+#include <boost/icl/discrete_interval.hpp>
+#include <boost/icl/interval_set.hpp>
+#include <solver/solver_library_export.hpp>
+
+#include <cassert>
+#include <cinttypes>
+#include <concepts>
+#include <iterator>
+#include <limits>
+#include <sstream>
+#include <string>
+#include <type_traits>
+#include <vector>
+
+namespace solver {
+
+
+template<std::integral ValueType = int32_t> SOLVER_LIBRARY_EXPORT class discrete_domain
+{
+private:
+  using interval_type = typename boost::icl::interval_set<ValueType>::interval_type;
+
+public:
+  using value_type = ValueType;
+  static constexpr ValueType MIN_VALUE = std::numeric_limits<ValueType>::min();
+
+  /// The value is smaller than max() because are bugs in isl when it deals with max()
+  static constexpr ValueType MAX_VALUE = std::numeric_limits<ValueType>::max() - 1;
+
+  constexpr discrete_domain() = default;
+  using iterator = typename boost::icl::interval_set<value_type>::element_const_iterator;
+  explicit constexpr discrete_domain(value_type value) : m_set() { *this = value; }
+
+
+  constexpr discrete_domain &operator=(value_type value)
+  {
+    validate_inserted_value(value);
+    m_set.clear();
+    m_set.insert(value);
+    return *this;
+  }
+
+  [[nodiscard]] constexpr bool is_universal() const { return boost::icl::contains(m_set, get_universal_interval()); }
+  [[nodiscard]] constexpr bool empty() const { return m_set.empty(); }
+  [[nodiscard]] constexpr bool is_singleton() const { return !is_universal() && m_set.size() == 1; }
+  [[nodiscard]] iterator begin() const { return elements_begin(m_set); }
+  [[nodiscard]] iterator end() const { return elements_end(m_set); }
+  constexpr void clear() { m_set.clear(); }
+  constexpr void erase(value_type value) { m_set.erase(value); }
+  constexpr void erase(std::pair<value_type, value_type> interval)
+  {
+    m_set.erase(make_interval(interval.first, interval.second));
+  }
+  constexpr void insert(value_type value)
+  {
+    validate_inserted_value(value);
+    m_set.insert(value);
+  }
+  constexpr void insert(std::pair<value_type, value_type> interval)
+  {
+    if (interval.second > MAX_VALUE) { throw std::invalid_argument("Value is too big"); }
+    if (interval.first > interval.second) { throw std::invalid_argument("Invalid interval"); }
+    m_set.insert(make_interval(interval.first, interval.second));
+  }
+
+  [[nodiscard]] constexpr bool contains(value_type value) const { return boost::icl::contains(m_set, value); }
+
+  friend constexpr value_type min(const discrete_domain &dom)
+  {
+    assert(!dom.empty());
+    return dom.m_set.begin()->lower();
+  }
+  friend constexpr value_type max(const discrete_domain &dom)
+  {
+    assert(!dom.empty());
+    return dom.m_set.rbegin()->upper();
+  }
+  friend bool operator==(const discrete_domain &left, const discrete_domain &right) = default;
+  friend bool operator==(const discrete_domain &left, value_type right)
+  {
+    return left.is_singleton() && left.contains(right);
+  }
+  [[nodiscard]] std::string to_string() const
+  {
+    std::ostringstream out;
+    out << *this;
+    return out.str();
+  }
+
+private:
+  static void validate_inserted_value(value_type value)
+  {
+    if (value > MAX_VALUE) { throw std::invalid_argument("Value is too big"); }
+  }
+
+  [[nodiscard]] static constexpr auto make_interval(value_type lower, value_type upper)
+  {
+    return interval_type::closed(lower, upper);
+  }
+
+  static constexpr interval_type get_universal_interval() { return make_interval(MIN_VALUE, MAX_VALUE); }
+  boost::icl::interval_set<value_type> m_set{ get_universal_interval() };
+};
+
+template<std::integral ValueType>
+[[nodiscard]] inline constexpr ValueType get_value(const discrete_domain<ValueType> &domain)
+{
+  return min(domain);
+}
+
+
+}// namespace solver
+
+#endif
---
+++ test/test_discrete_domain.cpp
@@ -0,0 +1,191 @@
+#include <algorithm>
+#include <cstdint>
+#include <limits>
+#include <solver/discrete_domain.hpp>
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_all.hpp>
+
+#include <iterator>
+#include <numeric>
+#include <random>
+#include <ranges>
+#include <span>
+#include <vector>
+
+using namespace solver;
+
+using Catch::Matchers::Equals;
+
+using uint8_domain = discrete_domain<uint8_t>;
+// NOLINTNEXTLINE(cert-err58-cpp)
+static const uint8_domain empty = [] {
+  uint8_domain ret;
+  ret.clear();
+  return ret;
+}();
+
+static const uint8_domain zero(0);// NOLINT(cert-err58-cpp)
+static const uint8_domain one(1);// NOLINT(cert-err58-cpp)
+static const uint8_domain biggest(uint8_domain::MAX_VALUE);// NOLINT(cert-err58-cpp)
+static const uint8_domain universal;// NOLINT(cert-err58-cpp)
+
+// NOLINTNEXTLINE(cert-err58-cpp)
+static const std::vector<uint8_t> uint8_full = [] {
+  std::vector<uint8_t> ret(uint8_domain::MAX_VALUE + 1, 0);
+  std::iota(ret.begin(), ret.end(), static_cast<uint8_t>(0));
+  return ret;
+}();
+
+template<typename ValueType>
+static void
+  domain_shuffled_insert(discrete_domain<ValueType> &domain, const std::vector<ValueType> &values, unsigned seed)
+{
+  auto shuffled_values = values;
+  std::mt19937 random(seed);
+  std::shuffle(shuffled_values.begin(), shuffled_values.end(), random);
+  for (const uint8_t val : shuffled_values) { domain.insert(val); }
+}
+
+// NOLINTNEXTLINE(readability-function-cognitive-complexity,cert-err58-cpp)
+TEST_CASE("Universal domain", "[int8_domain]")
+{
+  REQUIRE(uint8_domain().is_universal());
+  REQUIRE(!uint8_domain().is_singleton());
+  REQUIRE(!uint8_domain().empty());
+  REQUIRE(
+    (uint8_domain().contains(1) && uint8_domain().contains(0) && uint8_domain().contains(uint8_domain::MAX_VALUE)));
+  REQUIRE((min(uint8_domain()) == 0 && max(uint8_domain()) == uint8_domain::MAX_VALUE));
+  REQUIRE(!uint8_domain().contains(std::numeric_limits<uint8_t>::max()));
+}
+
+
+TEST_CASE("Empty domain", "[int8_domain]")
+{
+  REQUIRE(!empty.is_universal());
+  REQUIRE(!empty.is_singleton());
+  REQUIRE(empty.empty());
+  REQUIRE(!empty.contains(1));
+  REQUIRE(!empty.contains(0));
+  REQUIRE(!empty.contains(uint8_domain::MAX_VALUE));
+}
+
+TEST_CASE("Zero domain", "[int8_domain]")
+{
+  REQUIRE(!zero.is_universal());
+  REQUIRE(zero.is_singleton());
+  REQUIRE(!zero.empty());
+  REQUIRE((zero.contains(0) && !zero.contains(1)));
+  REQUIRE((min(zero) == 0 && max(zero) == 0 && get_value(zero) == 0));
+}
+
+TEST_CASE("One domain", "[int8_domain]")
+{
+  REQUIRE(!one.is_universal());
+  REQUIRE(one.is_singleton());
+  REQUIRE(!one.empty());
+  REQUIRE((!one.contains(0) && one.contains(1) && !one.contains(uint8_domain::MAX_VALUE)));
+  REQUIRE((min(one) == 1 && max(one) == 1 && get_value(one) == 1));
+}
+
+// NOLINTNEXTLINE(readability-function-cognitive-complexity)
+TEST_CASE("Biggest domain", "[int8_domain]")
+{
+  REQUIRE(!biggest.is_universal());
+  REQUIRE(biggest.is_singleton());
+  REQUIRE(!biggest.empty());
+  REQUIRE((!biggest.contains(0) && !biggest.contains(1) && biggest.contains(uint8_domain::MAX_VALUE)));
+  REQUIRE(min(biggest) == uint8_domain::MAX_VALUE);
+  REQUIRE(max(biggest) == uint8_domain::MAX_VALUE);
+  REQUIRE(get_value(biggest) == uint8_domain::MAX_VALUE);
+}
+
+TEST_CASE("Domain equality", "[int8_domain]")
+{
+  REQUIRE((empty != zero && empty != one && empty != uint8_domain() && empty == empty));
+  REQUIRE((zero != one && zero != uint8_domain() && zero == zero));
+  REQUIRE((one != uint8_domain() && one == one && biggest == biggest));
+  REQUIRE(uint8_domain() == uint8_domain());
+}
+
+// NOLINTNEXTLINE(readability-function-cognitive-complexity)
+TEST_CASE("Domain insertion", "[int8_domain]")
+{
+  uint8_domain entry = empty;
+  entry.insert(0);
+  REQUIRE(entry == zero);
+  entry.insert(0);
+  REQUIRE(entry == zero);
+
+  entry.insert(1);
+  REQUIRE_THAT(std::vector<uint8_t>(entry.begin(), entry.end()), Equals(std::vector<uint8_t>{ 0, 1 }));
+  entry.insert(uint8_domain::MAX_VALUE);
+  REQUIRE_THAT(std::vector(entry.begin(), entry.end()), Equals(std::vector<uint8_t>{ 0, 1, uint8_domain::MAX_VALUE }));
+  REQUIRE(entry != uint8_domain());
+  domain_shuffled_insert(entry, uint8_full, 2);
+  entry.insert(1);
+  entry.insert(0);
+  REQUIRE(entry == uint8_domain());
+
+  entry = empty;
+  entry.insert(1);
+  REQUIRE(entry == 1);
+  REQUIRE_THROWS_AS(entry = std::numeric_limits<uint8_t>::max(), std::invalid_argument);
+  REQUIRE(entry == 1);
+  entry.insert(0);
+  domain_shuffled_insert(entry, uint8_full, 3);// NOLINT(cert-msc32-c,cert-msc51-cpp)
+
+  REQUIRE(entry == uint8_domain());
+  REQUIRE_THROWS_AS(entry = std::numeric_limits<uint8_t>::max(), std::invalid_argument);
+  REQUIRE(entry == uint8_domain());
+}
+
+TEST_CASE("Domain forward iteration", "[int8_domain]")
+{
+  REQUIRE(empty.begin() == empty.end());
+  REQUIRE(one.begin() != one.end());
+  REQUIRE_THAT(std::vector(zero.begin(), zero.end()), Equals(std::vector<uint8_t>{ 0 }));
+  REQUIRE_THAT(std::vector(one.begin(), one.end()), Equals(std::vector<uint8_t>{ 1 }));
+  REQUIRE_THAT(std::vector(biggest.begin(), biggest.end()), Equals(std::vector<uint8_t>{ uint8_domain::MAX_VALUE }));
+  REQUIRE_THAT(std::vector(universal.begin(), universal.end()), Equals(uint8_full));
+}
+
+static std::vector<uint8_t> get_reverse(const uint8_domain &dom)
+{
+  std::vector<uint8_t> ret;
+  for (auto iter = dom.end(); iter != dom.begin();) {
+    --iter;
+    ret.push_back(*iter);
+  }
+  return ret;
+}
+
+TEST_CASE("Domain backward iteration", "[int8_domain]")
+{
+  uint8_domain::iterator iter;
+  iter = zero.begin();
+  REQUIRE(*iter == 0);
+  REQUIRE_THAT(get_reverse(zero), Equals(std::vector<uint8_t>{ 0 }));
+  REQUIRE_THAT(get_reverse(one), Equals(std::vector<uint8_t>{ 1 }));
+  REQUIRE_THAT(get_reverse(biggest), Equals(std::vector<uint8_t>{ uint8_domain::MAX_VALUE }));
+  auto reverse_full = uint8_full;
+  std::reverse(reverse_full.begin(), reverse_full.end());
+  REQUIRE_THAT(get_reverse(universal), Equals(reverse_full));
+}
+
+// NOLINTNEXTLINE(readability-function-cognitive-complexity)
+TEST_CASE("Domain assignment", "[int8_domain]")
+{
+  REQUIRE((zero == uint8_domain(0) && one == uint8_domain(1)));
+  uint8_domain domain;
+  domain = 0;
+  REQUIRE(domain == zero);
+  domain = 1;
+  REQUIRE(domain == one);
+  domain = 0;
+  REQUIRE(domain == zero);
+  domain = uint8_domain::MAX_VALUE;
+  REQUIRE(domain == biggest);
+  REQUIRE_THROWS_AS(domain = std::numeric_limits<uint8_t>::max(), std::invalid_argument);
+  REQUIRE(domain == biggest);
+}
