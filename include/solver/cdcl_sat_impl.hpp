8d71377ed839f24de1203d754fcbda1af4fd546d
Michael Veksler <mickey.veksler@gmail.com>
2023-12-01 22:21:11 +0200

    Split up the remainder of cdcl_sat.hpp

--- include/solver/cdcl_sat_impl.hpp
+++ include/solver/cdcl_sat_impl.hpp
@@ -1,31 +1,15 @@
-#ifndef CDCL_SAT_HPP
-#define CDCL_SAT_HPP
+#ifndef CDCL_SAT_IMPL_HPP
+#define CDCL_SAT_IMPL_HPP
 
 #include "solver/cdcl_sat_class.hpp"
 #include "solver/cdcl_sat_clause_class.hpp"
-#include "solver/cdcl_sat_clause_impl.hpp"
 #include "solver/domain_utils.hpp"
 #include "solver/sat_types.hpp"
 #include <boost/numeric/conversion/cast.hpp>
 #include <solver/solver_library_export.hpp>
 #include <solver/state_saver.hpp>
 
-#include <algorithm>
-#include <array>
-#include <cassert>
-#include <cinttypes>
-#include <cstdint>
-#include <deque>
-#include <limits>
 #include <map>
-#include <optional>
-#include <ranges>
-#include <set>
-#include <stdexcept>
-#include <string>
-#include <tuple>
-#include <variant>
-#include <vector>
 
 #include "spdlog/spdlog.h"
 
@@ -87,181 +71,6 @@ void cdcl_sat<Strategy>::set_domain(variable_handle var, domain_type domain, cla
     }
   }
 }
-
-/**
- * @brief Data-structure and algorithm used for conflict-analysis,
- *
- * It contains the currently generated clause, in the form of literals.
- */
-template<cdcl_sat_strategy Strategy> struct cdcl_sat_conflict_analysis_algo
-{
-  using clause_handle = typename cdcl_sat<Strategy>::clause_handle;
-  using variable_handle = typename cdcl_sat<Strategy>::variable_handle;
-  using cdcl_sat = solver::cdcl_sat<Strategy>;
-  using level_t = typename solver::cdcl_sat<Strategy>::level_t;
-  using clause = cdcl_sat_clause<Strategy>;
-  using implication = typename solver::cdcl_sat<Strategy>::implication;
-  /**
-   * @brief Construct a new conflict analysis algo object
-   *
-   * @param solver_in  A reference to the solver that had this conflict.
-   * @param conflicting_clause_handle  The clause that detected the conflict.
-   */
-  cdcl_sat_conflict_analysis_algo(cdcl_sat &solver_in, clause_handle conflicting_clause_handle);
-
-  /**
-   * @brief Get the decision level of the latest nt-th literal in the generated clause.
-   *
-   *
-   * @param distance_from_latest The i-th oldest literal in the data-structure.
-   *                             0 is the oldest, 1 is the almost oldest, etc.
-   * @return The decision level of the i-th oldest literal.
-   */
-  [[nodiscard]] level_t get_level(unsigned distance_from_latest = 0) const
-  {
-    const variable_handle var = std::next(implication_depth_to_var.rbegin(), distance_from_latest)->second;
-    return solver.m_implications[var].level;
-  }
-  /**
-   * @brief Is this a unit clause?
-   *
-   * A unit-clause is a clause that will perform an implication, i.e., reduce a domain, if we backtrack to the correct
-   * level.
-   *
-   * @retval true if this is a unit-clause.
-   */
-  [[nodiscard]] bool is_unit() const
-  {
-    if (implication_depth_to_var.size() <= 1) { return true; }
-    return get_level() != get_level(1);
-  }
-  /**
-   * @brief Is this the empty clause, i.e., has no literals and implies the problem is UNSAT.
-   *
-   * @retval true if this is the empty clause.
-   */
-  [[nodiscard]] bool empty() const { return conflict_literals.empty(); }
-  /**
-   * @brief Get the number of literals in the currently generated clause.
-   *
-   * @return the number of literals
-   */
-  [[nodiscard]] size_t size() const { return conflict_literals.size(); }
-
-  /**
-   * @brief Perform a binary-clause resolution.
-   *
-   * Given the pivot_var, find which clause has implied its value, and resolve with that constraint, over this
-   * pivot_var.
-   *
-   * @param pivot_var The pivot-var whose reducer will be resolved with the current literals.
-   */
-  void resolve(variable_handle pivot_var);
-
-  /**
-   * @brief Add and populate a clause with the current list of literals.
-   *
-   * @return clause_handle The handle of the newly created clause.
-   */
-  [[nodiscard]] clause_handle create_clause() const
-  {
-    const auto ret = boost::numeric_cast<clause_handle>(solver.m_clauses.size());
-    clause &added = solver.add_clause();
-    for (auto [var_num, is_positive] : conflict_literals) { added.add_literal(var_num, is_positive); }
-    return ret;
-  }
-
-  /**
-   * @brief Get the variable with the latest literal in this data-structure .
-   *
-   * @return The handle of the latest implied-variable.
-   */
-  [[nodiscard]] variable_handle get_latest_implied_var() const { return implication_depth_to_var.rbegin()->second; }
-  [[nodiscard]] std::string to_string() const;
-
-  cdcl_sat &solver;
-  std::map<variable_handle, bool> conflict_literals;
-  std::map<variable_handle, variable_handle> implication_depth_to_var;
-};
-
-template<cdcl_sat_strategy Strategy> std::string cdcl_sat_conflict_analysis_algo<Strategy>::to_string() const
-{
-  std::string ret = "{";
-  const char *sep = "";
-  for (auto [var, is_positive] : conflict_literals) {
-    ret += sep;
-    const level_t level = solver.m_implications[var].level;
-    ret += is_positive ? std::to_string(var) : '-' + std::to_string(var);
-    ret += '@' + std::to_string(level);
-    sep = ", ";
-  }
-  ret += '}';
-  return ret;
-}
-
-} // namespace solver
-
-
-template<solver::cdcl_sat_strategy Strategy> struct fmt::formatter<solver::cdcl_sat_conflict_analysis_algo<Strategy>> : fmt::formatter<std::string> {
-  template<typename FormatContext>
-  auto format(const solver::cdcl_sat_conflict_analysis_algo<Strategy> &algo, FormatContext &ctx)
-  {
-    return formatter<std::string>::format(algo.to_string(), ctx);
-  }
-};
-
-
-namespace solver {
-
-template<cdcl_sat_strategy Strategy>
-cdcl_sat_conflict_analysis_algo<Strategy>::cdcl_sat_conflict_analysis_algo(cdcl_sat &solver_in,
-  clause_handle conflicting_clause_handle)
-  : solver(solver_in)
-{
-  const clause &conflicting_clause = solver.m_clauses.at(conflicting_clause_handle);
-  log_info(
-    solver,
-    "initiating conflict analysis with conflicting_clause {}={}",
-    conflicting_clause_handle,
-    conflicting_clause);
-  for (typename clause::literal_index_t literal_num = 0; literal_num != conflicting_clause.size(); ++literal_num) {
-    const variable_handle var = conflicting_clause.get_variable(literal_num);
-    const variable_handle implication_depth = solver.m_implications[var].implication_depth;
-    if (implication_depth == 0) { continue; }
-    [[maybe_unused]] const auto [literal_iter, was_inserted] =
-      conflict_literals.emplace(var, conflicting_clause.is_positive_literal(literal_num));
-    assert(was_inserted);// NOLINT
-    implication_depth_to_var.emplace(implication_depth, var);
-  }
-  log_info(solver, "cl={}", *this);
-}
-
-template<cdcl_sat_strategy Strategy> void cdcl_sat_conflict_analysis_algo<Strategy>::resolve(variable_handle pivot_var)
-{
-  const implication imp = solver.m_implications.at(pivot_var);
-  const clause &prev_clause = solver.m_clauses.at(imp.implication_cause);
-  log_info(solver, "Resolving with {}={}", imp.implication_cause, prev_clause);
-  for (typename clause::literal_index_t literal_num = 0; literal_num != prev_clause.size(); ++literal_num) {
-    const bool is_positive = prev_clause.is_positive_literal(literal_num);
-    const variable_handle var = prev_clause.get_variable(literal_num);
-    const variable_handle implication_depth = solver.m_implications[var].implication_depth;
-    if (implication_depth == 0) { continue; }
-    if (var == pivot_var) {
-      assert(conflict_literals.at(var) != is_positive);
-      conflict_literals.erase(var);
-      implication_depth_to_var.erase(implication_depth);
-    } else {
-      auto [insert_iter, was_inserted] = conflict_literals.try_emplace(var, is_positive);
-      if (was_inserted) {
-        implication_depth_to_var.emplace(implication_depth, var);
-      } else {
-        assert(insert_iter->second == is_positive);
-      }
-    }
-  }
-  log_info(solver, "cl={}", *this);
-}
-
 template<cdcl_sat_strategy Strategy>
 auto cdcl_sat<Strategy>::analyze_conflict(clause_handle conflicting_clause)
   -> std::optional<std::pair<level_t, clause_handle>>
