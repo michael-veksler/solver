0e93fbb43e98ea54f692eeb6175e50d512aa0583
Michael Veksler <mickey.veksler@gmail.com>
2024-07-07 18:43:09 +0300

    linting + unit testing failure due to probabilities.

--- test/test_int_to_bool.cpp
+++ test/test_int_to_bool.cpp
@@ -13,7 +13,7 @@
 using namespace solver;
 using namespace solver::testing;
 
-class random_binary_domain_strategy  // NOLINT(cert-msc32-c,cert-msc51-cpp)
+class random_binary_domain_strategy
 {
 public:
     using domain_type = binary_domain;
@@ -34,8 +34,10 @@ public:
     {
         size_t num_important = m_important_variables.size();
         while (num_important > 0) {
+            // NOLINTNEXTLINE(misc-const-correctness) - overcome clang implementation bug
             std::uniform_int_distribution<literal_index_t> dist(0, static_cast<literal_index_t>(num_important-1));
             const size_t candidate_index = dist(m_random);
+
             const literal_index_t candidate_var = m_important_variables[candidate_index];
             if (m_is_free_var(candidate_var)) {
 
@@ -58,7 +60,7 @@ public:
         m_is_free_var = std::move(is_free_var);
     }
 private:
-    std::mt19937 m_random;
+    std::mt19937 m_random{std::random_device()()};
     std::vector<literal_index_t> m_important_variables;
     std::function<bool(literal_index_t)> m_is_free_var;
 };
@@ -91,10 +93,10 @@ TEST_CASE("multi value domain", "[int_to_bool]")  // NOLINT(readability-function
 {
     using domain_type =discrete_domain<uint8_t>;
     using value_type = domain_type::value_type;
-    const domain_type domain = {0, 1, 2, 10, 11, 12, domain_type::MAX_VALUE};
+    const domain_type domain = {0, 1, 2, 10, 11, domain_type::MAX_VALUE};
     std::map<uint8_t, unsigned> counts;
 
-    const size_t num_samples = domain.size() * 40;
+    const size_t num_samples = domain.size() * 100;
     for (unsigned seed = 1; seed <= num_samples; ++seed) {
         using strategy = random_binary_domain_strategy;
         cdcl_sat<strategy> solver;
@@ -113,7 +115,7 @@ TEST_CASE("multi value domain", "[int_to_bool]")  // NOLINT(readability-function
         counts[value]++;
     }
     const size_t average_count = num_samples / domain.size();
-    const size_t margin_percent = 35;
+    const size_t margin_percent = 20;
     const size_t margin = average_count * margin_percent / 100;
     const size_t lb_count = average_count - margin;
     const size_t ub_count = average_count + margin;
